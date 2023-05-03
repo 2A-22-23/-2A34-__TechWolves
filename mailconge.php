461bb807e563d85bc0c7da2cb93f7938900ac960
syrine0219 <100877723+syrine0219@users.noreply.github.com>
2023-05-03 10:55:59 +0100

    Add files via upload

---
+++ mailconge.php
@@ -0,0 +1,33 @@
+<?php
+require_once '../Model/conge.php';
+require_once '../Controller/congeC.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once 'phpmailer/src/Exception.php';
+require_once 'phpmailer/src/PHPMailer.php';
+require_once 'phpmailer/src/SMTP.php';
+session_start();
+if(isset($_GET["id"]))
+{
+$congeC= new congeC();
+$list=$congeC-> findconge2($_GET["id"]);
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'syrinebznamara8@gmail.com';
+    $mail->Password = 'mxjfeecvmjckyajl';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->setFrom('syrinebznamara8@gmail.com');
+    $mail->addAddress('syrine.benamara@esprit.tn');
+    $mail->isHTML(true);
+    $mail->Subject = 'conge';
+    $mail->Body = 'votre demande du conge de ' .$list["date_debut"]. ' à ' .$list["date_fin"]. ' a ete accepteé';
+    $mail->send();
+    $mail->SMTPDebug = 2;
+    echo "Mail envoyé";
+    header('location: listconge.php');
+}
+   ?>
